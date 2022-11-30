6ee3be25e7a9e20acc69ae313881a09a9e639889
aniecres <111096788+aniecres@users.noreply.github.com>
2022-11-29 21:58:29 -0500

    update readme

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # Readme-Generator
+
+## Description 
+
+To be able to help the user create a README.md file with already having the structure setup, in hopes that the user can focus on inputing the content correctly. While using this application to better help the user, it will also help them as they can use this template as a reference for future readme file uses. 
+
+## Installation
+To help create this program, Node.Js and Inquirer practiced. As well as npm package dependencies was installed by npm install in your terminal. 
+
+## Usage
+[Insert instructional video here]
--- index.js
+++ index.js
@@ -2,18 +2,29 @@
 const inquirer = require('inquirer');
 const fs = require('fs');
 
+
 // TODO: Create an array of questions for user input
 const questions = [
     {
         type: 'input',
-        name: 'project title',
+        name: 'title',
         message: 'What is the name of your project?',
       },
       {
         type: 'input',
-        name: 'location',
-        message: 'Where are you located?'
+        name: 'description',
+        message: 'Write a short description explaining your project.'
       },
+      {
+        type: 'input',
+        name: 'table',
+        message: 'If you choose to, write a Table of Contents'
+      },
+      {
+        type: 'input',
+        name: 'instillation',
+        message: ''
+      }
 ];
 
 // TODO: Create a function to write README file
