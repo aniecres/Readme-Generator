4aa7dd9f6076902210d50911b6380fc1face589d
aniecres <111096788+aniecres@users.noreply.github.com>
2022-11-30 19:58:31 -0500

    generateMarkdown code

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,12 +1,24 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
 function renderLicenseBadge(license) {
-  let renderLicenseBadge 
-}
+  let yourLicense  = '';
+  if (license === 'MIT') {
+    yourLicense = '![License MIT](https://img.shields.io/badge/License-MIT-yellow.svg)'
+  } else if (license === 'GPLv3') {
+    yourLicense = `![GPLv3 license](https://img.shields.io/badge/License-GPLv3-blue.svg)`
+  } else if (license === 'none') {
+    yourLicense = '';
+  }
+
+  return yourLicense;
+
+  }
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+function renderLicenseLink(license) {
+  
+}
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
